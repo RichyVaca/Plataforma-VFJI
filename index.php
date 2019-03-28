753fd68287f5b1bc67425a9a5750559daa5b74b7
DrupiMX <47230810+DrupiMX@users.noreply.github.com>
2019-03-28 17:34:30 -0600

    PHP
    Eliminado el archivo de index.html por index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
     <title>VFJI</title>
    </head>
    <link rel="stylesheet" href="css/estilo.css">
-   
+
 <body class="text-center">
     <div class="container-fluid"></div>
         <form action="" class="form-signing"></form>
@@ -16,11 +16,17 @@
 
             <h1 class="text-center">Plataforma VFJI</h1>
             <h3 class="text-center">Iniciar sesión</h3>
-        
+
             <input class="form-control formlogin" type="text" name="Usuario" placeholder="Usuario" required autofocus></input>
             <input class="form-control formlogin" type="password" name="psw" placeholder="Contraseña" required autofocus></input>
             <button onclick= location.href='Ventana_Menu.html' class="btn btn-block"  type="button">Iniciar Sesión</button>
             <button onClick= location.href='Registro.html' class="btn btn-block" type="button">Registrarse</button>
         </form>
+
+        <!-- Aquí hacemos la conexión con la base de datos -->
+        <?php
+        
+         ?>
+
 </body>
 </html>
