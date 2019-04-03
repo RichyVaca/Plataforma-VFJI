48419a6212af06345a9e6365b354930b4322a109
Ediercuz <lemurnews@hotmail.com>
2019-04-02 22:28:58 -0600

    formulario alumnos
    captura los que son tipos de texto como lo es la curp ,nombre y telefono , tambien el genero que es tipo radio, solo falta el boton de guardar.

--- php/alumnos.php
+++ php/alumnos.php
@@ -6,30 +6,24 @@
     <title></title>
   </head>
   <body>
-  <div class="container-fluid row">
-    <?php $resultado = $_POST;?>
-      <?php $curp = $resultado['curp'];    ?>
-      <?php $nombre = $resultado['curp']; ?>
 
-      <?php
-          // validar inputs
-          if(! (filter_has_var(INPUT_POST, 'curp') &&
-                (strlen(filter_input(INPUT_POST,'curp')) > 0))) {
-            echo "la curp es obligatorio";
-          } else {    ?>
-             <?php echo $curp; ?></p>
-          <?php } ?>
+        <?php
+      $curp = $_POST['curp'];
+      $nombre= $_POST['nombre'];
+      $telefono=$_POST['telefono'];
+      $correo= $_POST['correo'];
 
+      echo $curp . $nombre . $telefono . $correo;
 
-          <?php if(!isset($nombre) || trim($nombre) != '') { ?>
-             <?php echo $nombre; ?></p>
-          <?php } else {
-            echo "El apellido es obligatorio";
-      }  ?>
+         ?>
 
-      <hr>
+         <hr>
+        <?php
+        echo $_POST['optradio'];
 
-</div>
+        }
+
+         ?>
 
   </body>
 </html>
