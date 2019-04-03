ebefe226b63d1906031c08136b6ad96a76a16c39
Ediercuz <lemurnews@hotmail.com>
2019-04-02 21:56:48 -0600

    gfdg
    dfgfg

--- php/alumnos.php
+++ php/alumnos.php
@@ -1,23 +1,35 @@
 
-
-<?php $resultado = $_POST;?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <div class="container-fluid row">
+    <?php $resultado = $_POST;?>
       <?php $curp = $resultado['curp'];    ?>
-      <?php $nombre = $resultado['nombre']; ?>
+      <?php $nombre = $resultado['curp']; ?>
 
       <?php
-            // validar inputs
-            if(! (filter_has_var(INPUT_POST, 'curp') &&
-                  (strlen(filter_input(INPUT_POST,'curp')) > 0))) {
-              echo "El nombre es obligatorio";
-            } else {    ?>
-                <p>Curp: <?php echo $curp; ?></p>
-            <?php } ?>
+          // validar inputs
+          if(! (filter_has_var(INPUT_POST, 'curp') &&
+                (strlen(filter_input(INPUT_POST,'curp')) > 0))) {
+            echo "la curp es obligatorio";
+          } else {    ?>
+             <?php echo $curp; ?></p>
+          <?php } ?>
 
 
-            <?php if(!isset($nombre) || trim($nombre) != '') { ?>
-              <p>Nombre: <?php echo $nombre; ?></p>
-            <?php } else {
-              echo "El apellido es obligatorio";
-        }  ?>
+          <?php if(!isset($nombre) || trim($nombre) != '') { ?>
+             <?php echo $nombre; ?></p>
+          <?php } else {
+            echo "El apellido es obligatorio";
+      }  ?>
 
-      
+      <hr>
+
+</div>
+
+  </body>
+</html>
