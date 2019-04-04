e71c5a5e002be1ad8195bc9aff8a05f69947c202
Ricardo Hiram Morales Vaca <32494582+RichyVaca@users.noreply.github.com>
2019-04-04 16:38:50 -0600

    Update Registro Alumnos
    Detalle form

--- php/alumnos.php
+++ php/alumnos.php
@@ -1,26 +1,10 @@
+<?php
+  $curp = ['curp'];
+  $nombre= ['nombre'];
+  $telefono=['telefono'];
+  $correo=['correo'];
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+  echo $curp . $nombre . $telefono . $correo;
+  echo ['optradio'];
 
-        <?php
-      $curp = $_POST['curp'];
-      $nombre= $_POST['nombre'];
-      $telefono=$_POST['telefono'];
-      $correo= $_POST['correo'];
-
-      echo $curp . $nombre . $telefono . $correo;
-
-         ?>
-
-         <hr>
-        <?php
-          echo $_POST['optradio'];
-         ?>
-
-  </body>
-</html>
+?>
